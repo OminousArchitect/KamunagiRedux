3b98ecff2d29389acf1280210586bdcd4eeae79e
Bubbet <bubbet@bubbet.dev>
2024-09-01 19:56:44 -0400

    Start building survivor

---
+++ Data/Asset.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using RoR2;
+using RoR2.ContentManagement;
+using UnityEngine;
+
+namespace KamunagiOfChains.Data {
+    public abstract class Asset {       
+        public static Dictionary<string, object> Objects = new Dictionary<string, object>();
+        public static Dictionary<Type, Asset> Assets = new Dictionary<Type, Asset>();
+
+        public static ContentPack BuildContentPack()
+        {
+            var result = new ContentPack();
+        
+            var items = new List<ItemDef>();
+            var bodies = new List<GameObject>();
+            var survivors = new List<SurvivorDef>();
+            var unlockables = new List<UnlockableDef>();
+            
+            foreach (var type in Assembly.GetCallingAssembly().GetTypes())
+            {
+                if (!typeof(Asset).IsAssignableFrom(type)) continue;
+                var instance = (Asset) Activator.CreateInstance(type);
+                Assets[type] = instance;
+                if (instance is IUnlockable unlockable)
+                {
+                    var obj = unlockable.BuildObject();
+                    Objects[type.Name + "_" + nameof(IUnlockable)] = obj;
+                    unlockables.Add(obj);
+                }
+                if (instance is IItem item)
+                {
+                    var obj = item.BuildObject();
+                    Objects[type.Name + "_" + nameof(IItem)] = obj;
+                    items.Add(obj);
+                }
+                if (instance is IBodyDisplay body)
+                {
+                    var obj = body.BuildObject();
+                    Objects[type.Name + nameof(IBodyDisplay)] = obj;
+                }
+                if (instance is IBody body)
+                {
+                    var obj = body.BuildObject();
+                    Objects[type.Name + nameof(IBody)] = obj;
+                    bodies.Add(obj);
+                }
+                if (instance is ISurvivor survivor)
+                {
+                    var name = type.Name;
+                    var obj = survivor.BuildObject();
+                    obj.name = name + "SurvivorDef";
+                    Objects[name + "_" + nameof(ISurvivor)] = obj;
+                    survivors.Add(obj);
+                }
+            }
+
+            result.itemDefs.Add(items.ToArray());
+            result.bodyPrefabs.Add(bodies.ToArray());
+            result.survivorDefs.Add(survivors.ToArray());
+            result.unlockableDefs.Add(unlockables.ToArray());
+            return result;
+        }
+        
+        public static T? LoadAsset<T>(string assetPath) where T : Object
+        {
+            if (assetPath.StartsWith("addressable:"))
+            {
+                return Addressables.LoadAssetAsync<T>(assetPath["addressable:".Length..]).WaitForCompletion();
+            }
+
+            if (assetPath.StartsWith("bundle:"))
+            {
+                return !KamunagiOfChainsPlugin.Bundle ? null : KamunagiOfChainsPlugin.Bundle!.LoadAsset<T>(assetPath["bundle:".Length..]);
+            }
+            
+            if (assetPath.StartsWith("legacy:"))
+            {
+                return LegacyResourcesAPI.Load<T>(assetPath["legacy:".Length..]);
+            }
+
+            return null;
+        }
+
+        public static bool TryGetAsset<T>(out T asset) where T : Asset
+        {
+            if (Assets.TryGetValue(typeof(T), out var foundAsset))
+            {
+                asset = (T)foundAsset;
+                return true;
+            }
+
+            asset = default!;
+            return false;
+        }
+        
+        public static bool TryGetGameObject<T, T2>(out GameObject asset) where T2 : IGameObject
+        {
+            if (Objects.TryGetValue(typeof(T).Name + "_" + typeof(T2).Name, out var foundAsset))
+            {
+                asset = (GameObject) foundAsset;
+                return true;
+            }
+            asset = default!;
+            return false;
+        }
+        
+        private static object GetObjectOrThrow<T>(Asset asset)
+        {
+            var name = asset.GetType().Name;
+            var targetTypeName = typeof(T).Name;
+            if (Objects.TryGetValue(name + "_" + targetTypeName, out var result))
+            {
+                return result!;
+            }
+            throw new AssetTypeInvalidException($"{name} is not of type {targetTypeName}");
+        }
+        
+        public static implicit operator ItemDef(Asset asset) => (ItemDef) GetObjectOrThrow<IItem>(asset);
+        public static implicit operator UnlockableDef(Asset asset) => (UnlockableDef) GetObjectOrThrow<IUnlockable>(asset);
+        public static implicit operator SurvivorDef(Asset asset) => (SurvivorDef) GetObjectOrThrow<ISurvivor>(asset);
+    }
+
+    public class AssetTypeInvalidException : Exception
+    {
+        public AssetTypeInvalidException(string message): base(message) {}
+    }
+
+    public interface IGameObject {}
+    public interface IBody : IGameObject
+    {
+        public abstract GameObject BuildObject();
+    }
+    public interface IBodyDisplay : IGameObject
+    {
+        public abstract GameObject BuildObject();
+    }
+    public interface ISurvivor
+    {
+        public abstract SurvivorDef BuildObject();
+    }
+    public interface IItem {
+        public abstract string nameToken { get; }
+        public virtual ItemDef BuildObject()
+        {
+            var item = ScriptableObject.CreateInstance<ItemDef>();
+            item.nameToken = nameToken;
+            return item;
+        }
+    }
+    public interface IUnlockable
+    {
+        public abstract UnlockableDef BuildObject();
+    }
+}
---
+++ Data/Bodies/Kamunagi.cs
@@ -0,0 +1,93 @@
+using R2API;
+using RoR2;
+using UnityEngine;
+
+namespace KamunagiOfChains.Data.Bodies {
+    public class Kamunagi : Asset, IBody, IBodyDisplay, ISurvivor, IModel {
+        GameObject IModel.BuildObject()
+        {
+            var model = LoadAsset<GameObject>("bundle:mdlKamunagi");
+            var characterModel = model.GetComponent<CharacterModel>() || model.AddComponent<CharacterModel>();
+            var childLocator = model.GetComponent<ChildLocator>();
+            
+            RendererInfo RenderInfoFromChild(GameObject child, dontHopoo = false)
+            {
+                var renderer = child.GetComponent<Renderer>();
+                return new CharacterModel.RendererInfo()
+                {
+                    renderer = renderer,
+                    material = !dontHopoo ? renderer.material.SetHopooMaterial() : renderer.material,
+                    ignoreOverlays = false,
+                    defaultShadowCastingMode = ShadowCastingMode.On,
+                }
+            }
+            var voidCrystalMat = LoadAsset<Material>("addressable:RoR2/DLC1/voidstage/matVoidCrystal.mat");
+            characterModel.baseRenderInfos = [
+                    new CharacterModel.RendererInfo()
+                    {
+                        renderer = childLocator.FindChild("S Cloth01").GetComponent<Renderer>(),
+                        material = voidCrystalMat,
+                        ignoreOverlays = false,
+                        defaultShadowCastingMode = ShadowCastingMode.On,
+                    },
+                    new CharacterModel.RendererInfo()
+                    {
+                        renderer = childLocator.FindChild("U Cloth01").GetComponent<Renderer>(),
+                        material = voidCrystalMat,
+                        ignoreOverlays = false,
+                        defaultShadowCastingMode = ShadowCastingMode.On,
+                    },
+                    RenderInfoFromChild(childLocator.FindChild("S Body")),
+                    RenderInfoFromChild(childLocator.FindChild("U Body")),
+                    RenderInfoFromChild(childLocator.FindChild("S Cloth02"), true),
+                    RenderInfoFromChild(childLocator.FindChild("U Cloth02"), true),
+                    RenderInfoFromChild(childLocator.FindChild("S Hair")),
+                    RenderInfoFromChild(childLocator.FindChild("U Hair")),
+                    RenderInfoFromChild(childLocator.FindChild("S Jewelry")),
+                    RenderInfoFromChild(childLocator.FindChild("U Jewelry")),
+                    RenderInfoFromChild(childLocator.FindChild("S HandItems")),
+                    RenderInfoFromChild(childLocator.FindChild("U HandItems")),
+                    RenderInfoFromChild(childLocator.FindChild("S Shoe")),
+                    RenderInfoFromChild(childLocator.FindChild("U Shoe")),
+                ]
+        }
+        
+        GameObject IBodyDisplay.BuildObject()
+        {
+            if (!TryGetGameObject<Kamunagi, IModel>(out var model)) throw new Exception("Model not loaded.");
+            var displayModel = PrefabAPI.InstantiateClone(model);
+            displayModel.GetComponent<Animator>().runtimeAnimatorController = LoadAsset<AnimationController>("animHenryMenu");
+            return displayModel;
+        }
+        
+        GameObject IBody.BuildObject()
+        {
+            if (!TryGetGameObject<Kamunagi, IModel>(out var model)) throw new Exception("Model not loaded.");
+            var bodyPrefab = LoadAsset<GameObject>("legacy:Prefabs/CharacterBodies/CommandoBody").InstantiateClone("NinesKamunagiBody");
+            bodyPrefab.GetComponent<ModelLocator>().modelTransform = model.transform;
+            
+            return bodyPrefab;
+        }
+
+        SurvivorDef ISurvivor.BuildObject()
+        {
+            var survivor = ScriptableObject.CreateInstance<SurvivorDef>();
+            survivor.primaryColor = new Color(0.592156863f, 0f, 0.964705882f);
+            survivor.displayNameToken = "NINES_KAMUNAGI_BODY_NAME";
+            survivor.descriptionToken = "NINES_KAMUNAGI_BODY_DESCRIPTION";
+            survivor.outroFlavorToken = "NINES_KAMUNAGI_BODY_OUTRO_FLAVOR";
+            survivor.mainEndingEscapeFailureFlavorToken = "NINES_KAMUNAGI_BODY_OUTRO_FAILURE";
+            survivor.desiredSortPosition = 100f;
+            
+            if (TryGetGameObject<Kamunagi, IBody>(out var body))
+                survivor.bodyPrefab = body;
+            
+            if (TryGetGameObject<Kamunagi, IBodyDisplay>(out var display))
+                survivor.displayPrefab = display;
+            
+            if (TryGetAsset<Kamunagi>(out var unlockable))
+                survivor.unlockableDef = unlockable;
+            throw new System.NotImplementedException();
+        }
+    }
+}

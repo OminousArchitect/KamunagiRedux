4a064a639fb6b92dd68a2e5f31c7f5eed8c81317
Bubbet <bubbet@bubbet.dev>
2024-09-03 13:02:32 -0600

    Async loading, yes it helps. theres an actual quarter to half second the game hitches before the loading screen because its loading stuff from disk

--- KamunagiOfChainsPlugin.cs
+++ KamunagiOfChainsPlugin.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using R2API;
 using R2API.Utils;
 using UnityEngine;
 
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: UnverifiableCode]
+
 namespace KamunagiOfChains
 {
-    [BepInDependency(DotAPI.PluginGUID), BepInDependency(DamageAPI.PluginGUID), BepInDependency(ColorsAPI.PluginGUID), BepInDependency(R2API.ContentManagement.R2APIContentManager.PluginGUID)]
+    //[BepInDependency(DotAPI.PluginGUID), BepInDependency(DamageAPI.PluginGUID), BepInDependency(ColorsAPI.PluginGUID), BepInDependency(R2API.ContentManagement.R2APIContentManager.PluginGUID)]
+    [BepInDependency(PrefabAPI.PluginGUID)]
     [NetworkCompatibility]
     [BepInPlugin(Guid, Name, Version)]
     [HarmonyPatch]
@@ -16,6 +26,11 @@ namespace KamunagiOfChains
         public const string AssetBundleName = "kamunagiassets";
         public const string SoundBankName = "KamunagiMusic.bnk";
         public static AssetBundle? Bundle;
+        public static string PluginPath;
+        public static KamunagiOfChainsPlugin Instance;
+
+        public static ManualLogSource Log;
+        public static bool soundBankQueued;
 
         public const string Guid = "com.Nines.Kamunagi";
         public const string Name = "KamunagiOfChains";
@@ -23,30 +38,37 @@ namespace KamunagiOfChains
 
         public void Awake()
         {
+            Instance = this;
+            Log = Logger;
+            Log.LogDebug("Harmony Patching");
             // Hook all the harmony attributes
             new Harmony(Info.Metadata.GUID).PatchAll();
 
+            Log.LogDebug("Getting Plugin Path");
             // Get the path of the dll
-            var pluginPath = System.IO.Path.GetDirectoryName(Info.Location);
-            if (pluginPath is null) return;
+            PluginPath = System.IO.Path.GetDirectoryName(Info.Location) ?? throw new InvalidOperationException("Failed to find path of plugin.");
 
+            Log.LogDebug("Loading Asset Bundle");
             // Load Assets
-            Bundle = AssetBundle.LoadFromFile(System.IO.Path.Combine(pluginPath, AssetBundleName));
-            
-            // Load Soundbank
-            if (Application.isBatchMode) return;
-            try
+            AssetBundle.LoadFromFileAsync(System.IO.Path.Combine(PluginPath, AssetBundleName)).completed += operation =>
             {
-                AkSoundEngine.AddBasePath(pluginPath);
-                var result = AkSoundEngine.LoadBank(SoundBankName, out _);
-                if (result != AKRESULT.AK_Success)
-                    Logger.LogError("SoundBank Load Failed: " + result);
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-            }
-            
+                Log.LogDebug("Bundle Loaded");
+                Bundle = (operation as AssetBundleCreateRequest)?.assetBundle;
+            };
+
+            Log.LogDebug("Finished Awake");
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(RoR2.WwiseUtils.SoundbankLoader), nameof(RoR2.WwiseUtils.SoundbankLoader.Start))]
+        public static void AddSoundbankToLoader(RoR2.WwiseUtils.SoundbankLoader __instance)
+        {
+            // Ensure the soundbank isn't added to each loader, but only one.
+            if (soundBankQueued) return;
+            Log.LogDebug("Soundbank Added To Queue");
+            AkSoundEngine.AddBasePath(PluginPath);
+            __instance.soundbankStrings = __instance.soundbankStrings
+                .AddItem(SoundBankName).ToArray();
+            soundBankQueued = true;
         }
     }
 }

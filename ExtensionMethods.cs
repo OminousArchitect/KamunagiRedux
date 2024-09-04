8968c5b1b0a9da7eb03bfd515a5307a4874615bd
Bubbet <bubbet@bubbet.dev>
2024-09-03 18:39:27 -0600

    array destructure extension method

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -61,22 +61,10 @@ namespace KamunagiOfChains
             return tempMat;
         }
 
-        public static void Deconstruct<T>(this T[] array, out T first, out T second, out T[] rest)
+        public static void Deconstruct<T>(this T[] array, out T first, out T[] rest)
         {
             first = array[0];
-            second = array[1];
-            rest = array[2..];
-        }
-        public static void Deconstruct<T>(this T[] array, out T first, out T second)
-        {
-            first = array[0];
-            second = array[1];
-        }
-
-        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out IList<T> rest) {
-            first = list.Count > 0 ? list[0] : default(T); // or throw
-            second = list.Count > 1 ? list[1] : default(T); // or throw
-            rest = list.Skip(2).ToList();
+            rest = array[1..];
         }
     }
 }

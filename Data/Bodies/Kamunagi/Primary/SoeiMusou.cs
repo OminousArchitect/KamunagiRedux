9184ddb392f2bc458297070f31e5b521b9c09403
Bubbet <bubbet@bubbet.dev>
2024-09-04 16:13:09 -0600

    finish state for SoeiMusou.cs

--- Data/Bodies/Kamunagi/Primary/SoeiMusou.cs
+++ Data/Bodies/Kamunagi/Primary/SoeiMusou.cs
@@ -1,5 +1,40 @@
+using EntityStates;
+using KamunagiOfChains.Data.Bodies.Kamunagi.OtherStates;
+using RoR2;
+using RoR2.Projectile;
+using UnityEngine;
+
 namespace KamunagiOfChains.Data.Bodies.Kamunagi.Primary
 {
+    class SoeiMusouState : BaseTwinState
+    {
+        public override int meterGain => 0;
+
+        public static GameObject muzzlePrefab =
+            Asset.LoadAsset<GameObject>("addressable:RoR2/DLC1/VoidSurvivor/VoidSurvivorBeamMuzzleflash.prefab")!;
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            if (characterMotor.isGrounded) StartAimMode();
+            AkSoundEngine.PostEvent("Play_voidman_m2_shoot", base.gameObject);
+            EffectManager.SimpleMuzzleFlash(muzzlePrefab, base.gameObject, twinMuzzle, false);
+            var aimRay = base.GetAimRay();
+            if (!isAuthority || !Asset.TryGetGameObject<SoeiMusou, IProjectile>(out var projectile)) return;
+            ProjectileManager.instance.FireProjectile(new FireProjectileInfo
+            {
+                crit = RollCrit(),
+                damage = characterBody.damage * 2.9f,
+                force = 500,
+                owner = gameObject,
+                position = aimRay.origin,
+                rotation = Quaternion.LookRotation(aimRay.direction),
+                projectilePrefab = projectile,
+                useSpeedOverride = true,
+                speedOverride = 105f,
+            });
+        }
+        public override InterruptPriority GetMinimumInterruptPriority() => InterruptPriority.Skill;
+    }
     class SoeiMusou : Asset, ISkill
     {
         

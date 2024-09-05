4e7de1d7682a46e49fbd4694c8a46cc2975d5971
Bubbet <bubbet@bubbet.dev>
2024-09-05 14:27:45 -0600

    EffectManagerKamunagi.cs

---
+++ EffectManagerKamunagi.cs
@@ -0,0 +1,29 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace KamunagiOfChains
+{
+    public static class EffectManagerKamunagi
+    {
+        public static EffectManagerHelper GetAndActivatePooledEffect(GameObject prefab, Transform parentTransform,
+            bool inResetLocal = false, EffectData? data = null)
+        {
+            var pooledEffect = RoR2.EffectManager.GetPooledEffect(prefab, parentTransform);
+            var gameObject = pooledEffect.gameObject;
+            if (inResetLocal)
+            {
+                gameObject.transform.localPosition = Vector3.zero;
+                gameObject.transform.localRotation = Quaternion.identity;
+                gameObject.transform.localScale = Vector3.one;
+            }
+            var effectComponent = gameObject.GetComponent<EffectComponent>();
+            if (data != null && effectComponent)
+            {
+                effectComponent.effectData = data.Clone();
+                pooledEffect.Reset(true);
+                pooledEffect.gameObject.SetActive(true);
+            }
+            return pooledEffect;
+        }
+    }
+}

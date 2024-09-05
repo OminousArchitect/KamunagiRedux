841c5733ab29531c04d28aa6fe9c10657964dd5a
Bubbet <bubbet@bubbet.dev>
2024-09-05 10:53:37 -0600

    IndicatorSpellState.cs

---
+++ Data/Bodies/Kamunagi/OtherStates/IndicatorSpellState.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace KamunagiOfChains.Data.Bodies.Kamunagi.OtherStates
+{
+    public class IndicatorSpellState : BaseTwinState
+    {
+        public virtual float duration => 5f;
+        public virtual float failedCastCooldown => 2f;
+        public virtual float indicatorScale => 3f;
+        private GameObject? indicator;
+        private bool wasActive;
+
+        public override void Update()
+        {
+            if (!isAuthority) return;
+            if (inputBank.GetAimRaycast(float.PositiveInfinity, out var hit))
+            {
+                if (indicator == null || !indicator)
+                {
+                    indicator = Object.Instantiate(EntityStates.Huntress.ArrowRain.areaIndicatorPrefab);
+                    indicator.transform.localScale = Vector3.one * indicatorScale;
+                }
+
+                indicator.transform.position = hit.point;
+                if (wasActive) return;
+                indicator.SetActive(true);
+                wasActive = true;
+                return;
+            }
+
+            if (indicator == null || !indicator || !wasActive) return;
+            indicator.SetActive(false);
+            wasActive = false;
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (!isAuthority || (fixedAge < duration && IsKeyDownAuthority())) return;
+            outer.SetNextStateToMain();
+        }
+
+        public virtual void Fire(Vector3 targetPosition)
+        {
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            if (!isAuthority) return;
+            if (indicator != null && indicator)
+            {
+                Destroy(indicator);
+                if (indicator.activeSelf)
+                {
+                    Fire(indicator.transform.position);
+                    return;
+                }
+            }
+            activatorSkillSlot.rechargeStopwatch = activatorSkillSlot.finalRechargeInterval - failedCastCooldown;
+        }
+    }
+}
--- Data/Bodies/Kamunagi/Utility/Mikazuchi.cs
+++ Data/Bodies/Kamunagi/Utility/Mikazuchi.cs
@@ -7,44 +7,66 @@ using RoR2.Projectile;
 using RoR2.Skills;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using Object = UnityEngine.Object;
 
 namespace KamunagiOfChains.Data.Bodies.Kamunagi.Utility
 {
-    public class MikazuchiState : BaseTwinState
+    public class MikazuchiState : IndicatorSpellState
     {
-        public Transform? muzzleTransform;
         public EffectManagerHelper? chargeEffectInstance;
-        public GameObject? indicator;
         public float projectileCount = 3f;
+        public override float duration => 5f;
+        public override float failedCastCooldown => 2f;
 
         public override void OnEnter()
         {
             base.OnEnter();
-            muzzleTransform = FindModelChild("MuzzleCenter");
+            var muzzleTransform = FindModelChild("MuzzleCenter");
             if (!muzzleTransform || !Asset.TryGetGameObject<Mikazuchi, IEffect>(out var muzzleEffect)) return;
             chargeEffectInstance = EffectManager.GetAndActivatePooledEffect(muzzleEffect, muzzleTransform, true);
         }
-
-        public override void Update()
+        
+        public override void Fire(Vector3 targetPosition)
         {
-            base.Update();
-            if (!isAuthority) return;
-            if (!inputBank.GetAimRaycast(float.PositiveInfinity, out var hit)) return;
-            if (indicator == null || !indicator)
+            base.Fire(targetPosition);
+            var blastAttack = new BlastAttack
             {
-                indicator = Object.Instantiate(EntityStates.Huntress.ArrowRain.areaIndicatorPrefab);
-                indicator.transform.localScale = Vector3.one * 3f;
+                attacker = gameObject,
+                baseDamage = damageStat * 8.5f,
+                baseForce = 1800,
+                crit = RollCrit(),
+                damageType = DamageType.Shock5s,
+                falloffModel = BlastAttack.FalloffModel.None,
+                procCoefficient = 1,
+                radius = 3f,
+                position = targetPosition,
+                attackerFiltering = AttackerFiltering.NeverHitSelf,
+                teamIndex = teamComponent.teamIndex
+            };
+            blastAttack.Fire();
+            if (Asset.TryGetGameObject<MikazuchiLightningStrike, IEffect>(out var effect))
+                EffectManager.SpawnEffect(effect, new EffectData()
+                {
+                    origin = targetPosition,
+                    scale = blastAttack.radius
+                }, true);
+
+            var xoro = new Xoroshiro128Plus(Run.instance.runRNG.nextUlong);
+            var spacingDegrees = 360f / projectileCount;
+            var forward = Vector3.ProjectOnPlane(inputBank.aimDirection, Vector3.up);
+            var centerPoint = targetPosition + Vector3.up * 2.5f;
+            for (var i = 0; i < projectileCount; i++)
+            {
+                ProjectileManager.instance.FireProjectile(Asset.GetGameObject<MikazuchiLightningOrb, IProjectile>(),
+                    centerPoint,
+                    Util.QuaternionSafeLookRotation(Quaternion.AngleAxis(spacingDegrees * i, Vector3.up) *
+                                                    forward),
+                    gameObject,
+                    damageStat,
+                    10f,
+                    RollCrit(),
+                    speedOverride: xoro.RangeInt(13, 28)
+                );
             }
-
-            indicator.transform.position = hit.point;
-        }
-
-        public override void FixedUpdate()
-        {
-            base.FixedUpdate();
-            if (!isAuthority || (fixedAge < 5f && IsKeyDownAuthority())) return;
-            outer.SetNextStateToMain();
         }
 
         public override void OnExit()
@@ -52,56 +74,6 @@ namespace KamunagiOfChains.Data.Bodies.Kamunagi.Utility
             base.OnExit();
             if (chargeEffectInstance != null)
                 chargeEffectInstance.ReturnToPool();
-            if (!isAuthority) return;
-            if (indicator != null && indicator)
-            {
-                var indicatorPosition = indicator.transform.position;
-                var blastAttack = new BlastAttack
-                {
-                    attacker = gameObject,
-                    baseDamage = damageStat * 8.5f,
-                    baseForce = 1800,
-                    crit = RollCrit(),
-                    damageType = DamageType.Shock5s,
-                    falloffModel = BlastAttack.FalloffModel.None,
-                    procCoefficient = 1,
-                    radius = 3f,
-                    position = indicatorPosition,
-                    attackerFiltering = AttackerFiltering.NeverHitSelf,
-                    teamIndex = teamComponent.teamIndex
-                };
-                blastAttack.Fire();
-                if (Asset.TryGetGameObject<MikazuchiLightningStrike, IEffect>(out var effect))
-                    EffectManager.SpawnEffect(effect, new EffectData()
-                    {
-                        origin = indicatorPosition,
-                        scale = blastAttack.radius
-                    }, true);
-
-                var xoro = new Xoroshiro128Plus(Run.instance.runRNG.nextUlong);
-                var spacingDegrees = 360f / projectileCount;
-                var forward = Vector3.ProjectOnPlane(inputBank.aimDirection, Vector3.up);
-                var centerPoint = indicatorPosition + Vector3.up * 2.5f;
-                for (var i = 0; i < projectileCount; i++)
-                {
-                    ProjectileManager.instance.FireProjectile(Asset.GetGameObject<MikazuchiLightningOrb, IProjectile>(),
-                        centerPoint,
-                        Util.QuaternionSafeLookRotation(Quaternion.AngleAxis(spacingDegrees * i, Vector3.up) *
-                                                        forward),
-                        gameObject,
-                        damageStat,
-                        10f,
-                        RollCrit(),
-                        speedOverride: xoro.RangeInt(13, 28)
-                    );
-                }
-
-                Destroy(indicator);
-            }
-            else
-            {
-                activatorSkillSlot.rechargeStopwatch = activatorSkillSlot.finalRechargeInterval - 2f;
-            }
         }
 
         public override InterruptPriority GetMinimumInterruptPriority() => InterruptPriority.Skill;

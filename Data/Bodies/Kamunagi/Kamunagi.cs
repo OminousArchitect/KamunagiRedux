08164763a0f1b31db29c6a0c23951b684b4605bc
OminousArchitect <ominousalt@gmail.com>
2024-09-08 19:16:53 -0400

    -fixed chains size -added Senranzakura

--- Data/Bodies/Kamunagi/Kamunagi.cs
+++ Data/Bodies/Kamunagi/Kamunagi.cs
@@ -324,7 +324,7 @@ namespace KamunagiOfChains.Data.Bodies.Kamunagi
 			kamunagiChains.AddComponent<ModelAttachedEffect>();
 			kamunagiChains.transform.position = Vector3.zero;
 			kamunagiChains.transform.rotation = Quaternion.identity;
-			kamunagiChains.transform.localScale = new Vector3(0.4f, 0.8f, 0.4f);
+			kamunagiChains.transform.localScale = new Vector3(0.17f, 0.25f, 0.17f);
 			kamunagiChains.GetOrAddComponent<ParticleUVScroll>();
 
 			var comp = kamunagiChains.GetOrAddComponent<EffectComponent>();
--- Data/Bodies/Kamunagi/Utility/KamunagiSkillFamilyUtility.cs
+++ Data/Bodies/Kamunagi/Utility/KamunagiSkillFamilyUtility.cs
@@ -5,7 +5,7 @@ namespace KamunagiOfChains.Data.Bodies.Kamunagi.Utility
 	public class KamunagiSkillFamilyUtility : Asset, ISkillFamily
 	{
 		public Type[] GetSkillAssets() =>
-			new[] { typeof(Mikazuchi), typeof(AtuysTides), typeof(HonokasVeil), typeof(WoshisZone) };
+			new[] { typeof(Mikazuchi), typeof(AtuysTides), typeof(HonokasVeil), typeof(WoshisZone), typeof(JachdwaltStrikes) };
 
 		public string GetNameToken(GenericSkill skill) =>
 			skill.skillName == "SaraanaUtility" ? "NINES_SARAANA_UTILITY" : "NINES_URURUU_UTILITY";

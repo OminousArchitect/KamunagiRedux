92c9a3f8d47fa6314195f915b1a1071eb0ecbcc6
Bubbet <bubbet@bubbet.dev>
2024-09-12 21:12:19 -0600

    Revert "Comitting to master I think so we don't have 2 branches anymore"
    This reverts commit 348938b0

---
+++ KamunagiOfChainsPlugin.cs
@@ -0,0 +1,148 @@
+﻿global using static KamunagiOfChains.KamunagiOfChainsPlugin;
+using System.Collections;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using BepInEx.Logging;
+using ExtraSkillSlots;
+using HarmonyLib;
+using KamunagiOfChains.Data;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: UnverifiableCode]
+
+namespace KamunagiOfChains
+{
+	//[BepInDependency(DotAPI.PluginGUID), BepInDependency(DamageAPI.PluginGUID), BepInDependency(ColorsAPI.PluginGUID), BepInDependency(R2API.ContentManagement.R2APIContentManager.PluginGUID)]
+	[BepInDependency(PrefabAPI.PluginGUID)]
+	[BepInDependency(ColorsAPI.PluginGUID)]
+	[BepInDependency(DeployableAPI.PluginGUID)]
+	[BepInDependency(DotAPI.PluginGUID)]
+	[BepInDependency(ExtraSkillSlotsPlugin.GUID)]
+	[NetworkCompatibility]
+	[BepInPlugin(Guid, Name, Version)]
+	[HarmonyPatch]
+	public class KamunagiOfChainsPlugin : BaseUnityPlugin
+	{
+		public const string AssetBundleName = "kamunagiassets";
+		public const string SoundBankName = "KamunagiMusic.bnk";
+		public static AssetBundle? bundle;
+		public static string? pluginPath;
+		public static KamunagiOfChainsPlugin instance = null!;
+		public static ManualLogSource log = null!;
+		public static bool soundBankQueued;
+
+		public const string Guid = "com.Nines.Kamunagi";
+		public const string Name = "KamunagiOfChains";
+		public const string Version = "1.0.0";
+
+		public void Awake()
+		{
+			instance = this;
+			log = Logger;
+			log.LogDebug("Harmony Patching");
+			// Hook all the harmony attributes
+			new Harmony(Info.Metadata.GUID).PatchAll();
+
+			log.LogDebug("Getting Plugin Path");
+			// Get the path of the dll
+			pluginPath = System.IO.Path.GetDirectoryName(Info.Location) ??
+			             throw new InvalidOperationException("Failed to find path of plugin.");
+
+			log.LogDebug("Loading Asset Bundle");
+			// Load Assets
+			AssetBundle.LoadFromFileAsync(System.IO.Path.Combine(pluginPath, AssetBundleName)).completed += operation =>
+			{
+				log.LogDebug("Bundle Loaded");
+				bundle = (operation as AssetBundleCreateRequest)?.assetBundle;
+
+				log.LogDebug("Loading ContentPack");
+				ContentPackProvider.Initialize(Info.Metadata.GUID, Asset.BuildContentPack());
+			};
+
+			Language.collectLanguageRootFolders +=
+				folders => folders.Add(System.IO.Path.Combine(pluginPath, "Language"));
+
+			log.LogDebug("Finished Awake");
+		}
+
+		public static T? LoadAsset<T>(string assetPath) where T : UnityEngine.Object
+		{
+			if (assetPath.StartsWith("addressable:"))
+			{
+				return Addressables.LoadAssetAsync<T>(assetPath["addressable:".Length..]).WaitForCompletion();
+			}
+
+			if (assetPath.StartsWith("bundle:"))
+			{
+				return !bundle
+					? null
+					: bundle!.LoadAsset<T>(assetPath["bundle:".Length..]);
+			}
+
+			if (assetPath.StartsWith("legacy:"))
+			{
+				return LegacyResourcesAPI.Load<T>(assetPath["legacy:".Length..]);
+			}
+
+			return Addressables.LoadAssetAsync<T>(assetPath).WaitForCompletion();
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch(typeof(RoR2.WwiseUtils.SoundbankLoader), nameof(RoR2.WwiseUtils.SoundbankLoader.Start))]
+		public static void AddSoundbankToLoader(RoR2.WwiseUtils.SoundbankLoader __instance)
+		{
+			// Ensure the soundbank isn't added to each loader, but only one.
+			if (soundBankQueued) return;
+			log.LogDebug("Soundbank Added To Queue");
+			AkSoundEngine.AddBasePath(pluginPath);
+			__instance.soundbankStrings = __instance.soundbankStrings
+				.AddItem(SoundBankName).ToArray();
+			soundBankQueued = true;
+		}
+
+		private class ContentPackProvider : RoR2.ContentManagement.IContentPackProvider
+		{
+			private static RoR2.ContentManagement.ContentPack _contentPack = null!;
+			private static string _identifier = null!;
+			public string identifier => _identifier;
+
+			public IEnumerator LoadStaticContentAsync(RoR2.ContentManagement.LoadStaticContentAsyncArgs args)
+			{
+				//ContentPack.identifier = identifier;
+				args.ReportProgress(1f);
+				yield break;
+			}
+
+			public IEnumerator GenerateContentPackAsync(RoR2.ContentManagement.GetContentPackAsyncArgs args)
+			{
+				RoR2.ContentManagement.ContentPack.Copy(_contentPack, args.output);
+				//Log.LogError(ContentPack.identifier);
+				args.ReportProgress(1f);
+				yield break;
+			}
+
+			public IEnumerator FinalizeAsync(RoR2.ContentManagement.FinalizeAsyncArgs args)
+			{
+				args.ReportProgress(1f);
+				log.LogInfo("Contentpack finished");
+				yield break;
+			}
+
+			internal static void Initialize(string identifier, RoR2.ContentManagement.ContentPack pack)
+			{
+				_identifier = identifier;
+				_contentPack = pack;
+				RoR2.ContentManagement.ContentManager.collectContentPackProviders +=
+					provider => provider(new ContentPackProvider());
+			}
+		}
+	}
+}
---
+++ ParticleUVScroll.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+
+namespace KamunagiOfChains
+{
+	[ExecuteInEditMode]
+	public class ParticleUVScroll : MonoBehaviour
+	{
+		[SerializeField] private ParticleSystem particleSystem;
+
+		[SerializeField] private bool isSubEmmitter;
+
+		//[SerializeField]
+		//Vector2 beginningUVs;
+		//[SerializeField]
+		//Vector2 endUVs;
+		[SerializeField] private float endOffsetX;
+		[SerializeField] private float endOffsetY = -0.6f;
+		[SerializeField] private Material material;
+		[SerializeField] private List<int> shaderIDs;
+		[SerializeField] private float duration;
+		[SerializeField] private float offX;
+		[SerializeField] private float offY;
+		[SerializeField] private float delay;
+		[SerializeField] private float ltime;
+		[SerializeField] private float ptime;
+		private ParticleSystem parentSystem;
+		private float lifetime;
+		[SerializeField] private bool useLifetime = true;
+		[SerializeField] private AnimationCurve xCurve, yCurve;
+		[SerializeField] private bool useCurve = false;
+		private float curveTime;
+		[SerializeField] private float randomXStartPerc;
+		[SerializeField] private float randomYStartPerc;
+
+		private void OnEnable()
+		{
+			particleSystem = gameObject.GetComponent<ParticleSystem>();
+			var main = particleSystem.main;
+			ltime = 0;
+			if (isSubEmmitter)
+			{
+				parentSystem = gameObject.transform.parent.GetComponent<ParticleSystem>();
+				var parentSub = parentSystem.subEmitters;
+				var index = 0;
+				for (var i = 0; i < parentSub.subEmittersCount; i++)
+				{
+					if (parentSub.GetSubEmitterSystem(i) == particleSystem)
+					{
+						index = i;
+					}
+				}
+
+				var parentProp = parentSub.GetSubEmitterProperties(index);
+				if (useLifetime)
+				{
+					if (parentProp.HasFlag(ParticleSystemSubEmitterProperties.InheritLifetime))
+					{
+						lifetime = parentSystem.main.startLifetime.constant * main.startLifetime.constant;
+						delay = parentSystem.main.startDelay.constant + main.startDelay.constant;
+					}
+					else
+					{
+						lifetime = main.startLifetime.constant;
+						delay = parentSystem.main.startDelay.constant + main.startDelay.constant;
+					}
+				}
+				else
+				{
+					if (parentProp.HasFlag(ParticleSystemSubEmitterProperties.InheritDuration))
+					{
+						duration = parentSystem.main.duration * main.duration;
+						delay = parentSystem.main.startDelay.constant + main.startDelay.constant;
+					}
+					else
+					{
+						duration = main.duration;
+						delay = parentSystem.main.startDelay.constant + main.startDelay.constant;
+					}
+				}
+			}
+			else
+			{
+				if (useLifetime)
+				{
+					lifetime = main.startLifetime.constant;
+					delay = main.startDelay.constant;
+				}
+				else
+				{
+					duration = main.duration;
+					delay = main.startDelay.constant;
+				}
+			}
+
+
+			material = gameObject.GetComponent<ParticleSystemRenderer>().sharedMaterial;
+			shaderIDs = new List<int>();
+			if (material.HasProperty(Shader.PropertyToID("_MainTex")))
+				shaderIDs.Add(Shader.PropertyToID("_MainTex"));
+			if (material.HasProperty(Shader.PropertyToID("_BumpMap")))
+				shaderIDs.Add(Shader.PropertyToID("_BumpMap"));
+			if (material.HasProperty(Shader.PropertyToID("_MaskTex")))
+				shaderIDs.Add(Shader.PropertyToID("_MaskTex"));
+			if (material.HasProperty(Shader.PropertyToID("_EmissionMap")))
+				shaderIDs.Add(Shader.PropertyToID("_EmissionMap"));
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+			if (particleSystem.isPlaying)
+			{
+				if (isSubEmmitter)
+					ptime = parentSystem.time;
+				else
+					ptime = particleSystem.time;
+
+				//if (particleSystem.time >= delay)
+				//{
+
+				if (useLifetime)
+					ltime = ptime / lifetime;
+				else
+					ltime = ptime / duration;
+				if (useCurve)
+				{
+					offX = xCurve.Evaluate(ltime);
+					if (randomXStartPerc != 0)
+						offX += AddStartOffset(randomXStartPerc);
+					offY = yCurve.Evaluate(ltime);
+					if (randomYStartPerc != 0)
+						offY += AddStartOffset(randomYStartPerc);
+					foreach (var ID in shaderIDs)
+					{
+						if (material.GetTexture(ID))
+						{
+							material.SetTextureOffset(ID, new Vector2(offX, offY));
+						}
+					}
+				}
+				else
+				{
+					if (endOffsetX != 0)
+					{
+						offX = Mathf.Lerp(0, endOffsetX, ltime);
+						if (randomXStartPerc != 0)
+							offX += AddStartOffset(randomXStartPerc);
+					}
+
+					else
+						offX = 0;
+
+					if (endOffsetY != 0)
+					{
+						offY = Mathf.Lerp(0, endOffsetY, ltime);
+						if (randomYStartPerc != 0)
+							offY += AddStartOffset(randomYStartPerc);
+					}
+
+					else
+						offY = 0;
+
+					foreach (var ID in shaderIDs)
+					{
+						if (material.GetTexture(ID))
+						{
+							material.SetTextureOffset(ID, new Vector2(offX, offY));
+						}
+					}
+					//  }
+				}
+			}
+			else if (material.mainTextureOffset != Vector2.zero)
+			{
+				offX = 0;
+				offY = 0;
+				foreach (var ID in shaderIDs)
+				{
+					if (material.GetTexture(ID))
+					{
+						material.SetTextureOffset(ID, new Vector2(offX, offY));
+					}
+				}
+			}
+		}
+
+		private void OnDisable()
+		{
+			foreach (var ID in shaderIDs)
+			{
+				if (material.GetTexture(ID))
+					material.SetTextureOffset(ID, Vector2.zero);
+			}
+
+			shaderIDs.Clear();
+		}
+
+		private float AddStartOffset(float offsetPerc)
+		{
+			var whole = 1 / offsetPerc;
+			float rand = UnityEngine.Random.Range(0, (int)whole - 1);
+			return rand % whole * offsetPerc;
+		}
+	}
+}

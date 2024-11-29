bba692fab473735749bd581be28550fe63a41114
OminousArchitect <ominousalt@gmail.com>
2024-11-29 12:39:16 -0500

    debug zeal state

---
+++ Data/Bodies/Kamunagi/Primary/DebugZeal.cs
@@ -0,0 +1,44 @@
+﻿using EntityStates;
+using KamunagiOfChains.Data.Bodies.Kamunagi.OtherStates;
+using RoR2.Skills;
+using UnityEngine;
+
+namespace KamunagiOfChains.Data.Bodies.Kamunagi.Primary
+{
+	public class DebugZealState : BaseTwinState
+	{
+		public override int meterGain => 85;
+		private float duration = 0.2f;
+
+		public override void FixedUpdate()
+		{
+			base.FixedUpdate();
+			if (fixedAge >= duration)
+			{
+				outer.SetNextStateToMain();
+			}
+		}
+	}
+
+	public class DebugZeal : Concentric, ISkill
+	{
+		async Task<SkillDef> ISkill.BuildObject()
+		{
+			var skill = ScriptableObject.CreateInstance<SkillDef>();
+			skill.skillName = "Primary 0";
+			skill.skillNameToken = KamunagiAsset.tokenPrefix + "PRIMARY0_NAME";
+			skill.skillDescriptionToken = KamunagiAsset.tokenPrefix + "PRIMARY0_DESCRIPTION";
+			skill.icon = await LoadAsset<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png");
+			skill.activationStateMachineName = "Weapon";
+			skill.baseMaxStock = 1;
+			skill.baseRechargeInterval = 1f;
+			skill.interruptPriority = InterruptPriority.Any;
+			skill.cancelSprintingOnActivation = false;
+			skill.rechargeStock = 1;
+			skill.mustKeyPress = true;
+			return skill;
+		}
+		
+		IEnumerable<Type> ISkill.GetEntityStates() => new[] { typeof(DebugZealState) };
+	}
+}
